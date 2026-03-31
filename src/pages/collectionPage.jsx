2363912f28f6841f85ccd2f86fecfb7f5219683f
naveen <185893220+codamee@users.noreply.github.com>
2026-03-31 21:20:20 +0530

    feat : add check for empty collection

--- src/pages/collectionPage.jsx
+++ src/pages/collectionPage.jsx
@@ -20,11 +20,13 @@ const CollectionPage = () => {
                     <button className='px-4 py-2 w-full rounded font-semibold cursor-pointer bg-blue-600'>Home</button>
                 </Link>
             </nav>
-            <div className='flex justify-between items-center py-5 px-10'>
-                <p className='px-4 py-2 rounded font-semibold cursor-pointer border'>your collection</p>
-                <button onClick={() => { removeWholeCollection() }} className='px-4 py-2 rounded font-semibold cursor-pointer bg-red-600'>clear collection</button>
-            </div>
-            {(items.length===0) && <div className='p-10 text-xl'>🔥Nothing to show here ! Add some to collection ....</div>}
+
+            {(!items.length > 0) ?
+                <div className='p-10 text-xl'>🔥Nothing to show here ! Add some to collection ....</div> :
+                <div className='flex justify-between items-center py-5 px-10'>
+                    <p className='px-4 py-2 rounded font-semibold cursor-pointer border'>your collection</p>
+                    <button onClick={() => { removeWholeCollection() }} className='px-4 py-2 rounded font-semibold cursor-pointer bg-red-600'>clear collection</button>
+                </div>}
             <div className="grid grid-cols-4 gap-7 px-10 pb-10 justify-between ">
                 {items.map((item, index) => {
                     return <CollectionCard key={item.id || index} item={item} />
